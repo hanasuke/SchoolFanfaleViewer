d27a593ff4e2fe07b37e8e70bd3f14c45ec37fd8
naosuke <naosuke@live.jp>
2015-04-27 20:58:02 +0900

    first commit

---
+++ SchoolFanfaleViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SchoolFanfaleViewer
+//
+//  Created by naosuke on 4/27/15.
+//  Copyright (c) 2015 Naoki Hanakawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
